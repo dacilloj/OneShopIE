d61d210fd2651e72a2c05516fed90a94ef52a8a6
dacillo <johncarlodacillo123@gmail.com>
2022-11-10 20:26:55 +0000

    Add project files.

---
+++ OneShopIE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneShopIE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
